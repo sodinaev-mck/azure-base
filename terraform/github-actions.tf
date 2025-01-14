4df1632b954a4428ea15e524a26f37bea915a0b0
Sherzod Odinaev <Sherzod_Odinaev@mckinsey.com>
2025-01-14 08:58:25 -0500

    updated values for the repo

--- terraform/github-actions.tf
+++ terraform/github-actions.tf
@@ -1,16 +1,16 @@
 // Set env vars for GitHub Actions
 resource "github_actions_variable" "acr_repository_name" {
-  repository    = "azure-base-network"
+  repository    = "azure-base"
   variable_name = "AZURE_ACR_NAME"
   value         = azurerm_container_registry.acr.name
 }
 
 resource "github_actions_variable" "acr_login_server" {
-  repository    = "azure-base-network"
+  repository    = "azure-base"
   variable_name = "AZURE_ACR_LOGIN_SERVER"
   value         = azurerm_container_registry.acr.login_server
 }
 
 data "github_actions_registration_token" "default" {
-  repository = "azure-base-network"
+  repository = "azure-base"
 }
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">=1.0"
+  required_version = ">=1.10"
 
   required_providers {
     azurerm = ">= 4.0, <5.0"
