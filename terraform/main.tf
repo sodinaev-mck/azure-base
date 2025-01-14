799778cc6dffaaa458e106cf572e8716d4b2e786
Sherzod Odinaev <Sherzod_Odinaev@mckinsey.com>
2025-01-14 09:24:44 -0500

    gha

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,9 +15,14 @@ resource "azurerm_user_assigned_identity" "main" {
   name                = "sherzod-odinaev-sandbox-mi"
 }
 
+locals {
+  mi_roles = ["Reader", "AcrPull"]
+}
+
 // grant access to the Managed Identity to the resource group
 resource "azurerm_role_assignment" "main" {
+  for_each             = toset(local.mi_roles)
   scope                = azurerm_resource_group.main.id
-  role_definition_name = "Reader"
+  role_definition_name = each.value
   principal_id         = azurerm_user_assigned_identity.main.principal_id
 }
